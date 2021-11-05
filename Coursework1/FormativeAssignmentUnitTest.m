2b8dacdf50f714179249f5f529b9de944c74b68a
Reece Bell <rrb32@bath.ac.uk>
2021-11-05 12:17:29 +0000

    added missing files

--- Coursework1/FormativeAssignmentUnitTest.m
+++ Coursework1/FormativeAssignmentUnitTest.m
@@ -1,32 +1,27 @@
-%Similarities between diffusion and reaction local element matrices allow
-%lots of the provided code to be reused here:
-
 %% Test 1: test symmetry of the matrix
-% % Test that this matrix is symmetric. This is the same verification as for the
-% diffusion LEM's
+% % Test that this matrix is symmetric
 tol = 1e-14;
-lamda = 2; %diffusion coefficient
+D = 2; %diffusion coefficient
 eID=1; %element ID
 msh = OneDimLinearMeshGen(0,1,10);
 
-elemat = ReactionMatrixUni(lamda,eID,msh); %THIS IS THE FUNCTION YOU MUST WRITE
+elemat = LaplaceElemMatrix(D,eID,msh); %THIS IS THE FUNCTION YOU MUST WRITE
 
 assert(abs(elemat(1,2) - elemat(2,1)) <= tol)
 
 %% Test 2: test 2 different elements of the same size produce same matrix
 % % Test that for two elements of an equispaced mesh, as described in the
-% % lectures, the element matrices calculated are the same. This is the 
-%   same verification as for the diffusion LEM's
+% % lectures, the element matrices calculated are the same
 tol = 1e-14;
-lamda = 5; %diffusion coefficient
+D = 5; %diffusion coefficient
 eID=1; %element ID
 msh = OneDimLinearMeshGen(0,1,10);
 
-elemat1 = ReactionMatrix(lamda,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
+elemat1 = LaplaceElemMatrix(D,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
 
 eID=2; %element ID
 
-elemat2 = ReactionMatrix(lamda,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
+elemat2 = LaplaceElemMatrix(D,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
 
 diff = elemat1 - elemat2;
 diffnorm = sum(sum(diff.*diff));
@@ -34,16 +29,15 @@ assert(abs(diffnorm) <= tol)
 
 %% Test 3: test that one matrix is evaluted correctly
 % % Test that element 1 of the three element mesh problem described in the lectures
-% % the element matrix is evaluated correctly. This uses the example shown
-% % from tutorial sheet 3.
+% % the element matrix is evaluated correctly
 tol = 1e-14;
-lamda = 9; %reaction coefficient
+D = 2.5; %diffusion coefficient
 eID=1; %element ID
 msh = OneDimLinearMeshGen(0,1,3);
 
-elemat1 = LaplaceElemMatrix(lamda,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
+elemat1 = LaplaceElemMatrix(D,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
 
-elemat2 = [ 1 0.5; 1 0.5];
+elemat2 = [ 7.5 -7.5; -7.5 7.5];
 diff = elemat1 - elemat2; %calculate the difference between the two matrices
 diffnorm = sum(sum(diff.*diff)); %calculates the total squared error between the matrices
 assert(abs(diffnorm) <= tol)
@@ -51,11 +45,11 @@ assert(abs(diffnorm) <= tol)
 %% Test 4: test that different sized elements in a mesh are evaluted correctly - element 1
 % % Test that elements in a non-equally spaced mesh are evaluated correctly
 tol = 1e-14;
-lamda = 1; %diffusion coefficient
+D = 1; %diffusion coefficient
 eID=1; %element ID
 msh = OneDimSimpleRefinedMeshGen(0,1,5);
 
-elemat1 = LaplaceElemMatrix(lamda,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
+elemat1 = LaplaceElemMatrix(D,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
 
 elemat2 = [ 2 -2; -2 2];
 diff = elemat1 - elemat2; %calculate the difference between the two matrices
@@ -65,11 +59,11 @@ assert(abs(diffnorm) <= tol)
 %% Test 5: test that different sized elements in a mesh are evaluted correctly - element 4
 % % Test that elements in a non-equally spaced mesh are evaluated correctly
 tol = 1e-14;
-lamda = 1; %diffusion coefficient
+D = 1; %diffusion coefficient
 eID=4; %element ID
 msh = OneDimSimpleRefinedMeshGen(0,1,5);
 
-elemat1 = LaplaceElemMatrix(lamda,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
+elemat1 = LaplaceElemMatrix(D,eID,msh);%THIS IS THE FUNCTION YOU MUST WRITE
 
 elemat2 = [ 16 -16; -16 16];
 diff = elemat1 - elemat2; %calculate the difference between the two matrices
